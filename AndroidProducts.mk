d4780349aaf1accf3a0e43586dc581370f98ab25
Faktorall <verdenrot@gmail.com>
2026-09-02 10:57:44 +0200

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,9 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_dew.mk
+    $(LOCAL_DIR)/lineage_generic.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_dew-user \
-    twrp_dew-userdebug \
-    twrp_dew-eng
+    lineage_generic-user \
+    lineage_generic-userdebug \
+    lineage_generic-eng
